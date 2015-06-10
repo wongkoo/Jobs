c8b9e70298bb4e08ed44f176777887b960866c86
wongkoo <wangzhenhuip@163.com>
2015-06-10 10:05:42 +0800

    Add MCSwipeTableViewCell foundation

---
+++ Pods/Target Support Files/Pods-Jobs-MCSwipeTableViewCell/Pods-Jobs-MCSwipeTableViewCell-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_Jobs_MCSwipeTableViewCell : NSObject
+@end
+@implementation PodsDummy_Pods_Jobs_MCSwipeTableViewCell
+@end
---
+++ Pods/Target Support Files/Pods-Jobs/Pods-Jobs-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_Jobs : NSObject
+@end
+@implementation PodsDummy_Pods_Jobs
+@end
