d3e71243dae2a046b6a04e9bd094b764253800c1
wongkoo <wangzhenhuip@163.com>
2015-06-22 16:14:57 +0800

    Change details in IntroView

--- Jobs/IntroPageViewController.m
+++ Jobs/IntroPageViewController.m
@@ -11,10 +11,10 @@
 #import "AppDelegate.h"
 #import "AllListsViewController.h"
 
-static NSString * const sampleDescription1 = @"Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.";
-static NSString * const sampleDescription2 = @"Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore.";
-static NSString * const sampleDescription3 = @"Neque porro quisquam est, qui dolorem ipsum quia dolor sit amet, consectetur, adipisci velit, sed quia non numquam eius modi tempora incidunt ut labore et dolore magnam aliquam quaerat voluptatem.";
-static NSString * const sampleDescription4 = @"Nam libero tempore, cum soluta nobis est eligendi optio cumque nihil impedit.";
+static NSString * const sampleDescription1 = @"HR打电话的时候，我再也不会忘记当初报的是哪个职位了。";
+static NSString * const sampleDescription2 = @"灰色公司任务向右“短滑”恢复。";
+static NSString * const sampleDescription3 = @"灰色公司任务向右“长滑”永久删除";
+static NSString * const sampleDescription4 = @"简单，在细节中。向左滑开始我的计划";
 
 @interface IntroPageViewController ()
     
@@ -30,56 +30,45 @@ static NSString * const sampleDescription4 = @"Nam libero tempore, cum soluta no
 
 - (void)showIntro{
     EAIntroPage *page1 = [EAIntroPage page];
-    page1.title = @"Hello world";
+    page1.title = @"Hello Jobs";
+    page1.titleColor = [UIColor colorWithRed:255.0/255.0 green:77.0/255.0 blue:0.0 alpha:1];
     page1.desc = sampleDescription1;
+    page1.descColor = [UIColor colorWithRed:255.0/255.0 green:77.0/255.0 blue:0.0 alpha:1];
     page1.bgImage = [UIImage imageNamed:@"bg1"];
     page1.titleIconView = [[UIImageView alloc] initWithImage:[UIImage imageNamed:@"title1"]];
     
     EAIntroPage *page2 = [EAIntroPage page];
-    page2.title = @"This is page 2";
+    page2.title = @"Jobs";
+    page2.titleColor = [UIColor blackColor];
     page2.desc = sampleDescription2;
+    page2.descColor = [UIColor blackColor];
     page2.bgImage = [UIImage imageNamed:@"bg2"];
     page2.titleIconView = [[UIImageView alloc] initWithImage:[UIImage imageNamed:@"title2"]];
-//    page2.onPageDidAppear = ^{
-//        NSLog(@"Page 2 did appear block");
-//    };
     
     EAIntroPage *page3 = [EAIntroPage page];
-    page3.title = @"This is page 3";
+    page3.title = @"Jobs";
+    page3.titleColor = [UIColor blackColor];
     page3.desc = sampleDescription3;
+    page3.descColor = [UIColor blackColor];
     page3.bgImage = [UIImage imageNamed:@"bg3"];
     page3.titleIconView = [[UIImageView alloc] initWithImage:[UIImage imageNamed:@"title3"]];
     
     EAIntroPage *page4 = [EAIntroPage page];
-    page4.title = @"This is page 4";
+    page4.title = @"Jobs";
+    page4.titleColor = [UIColor blackColor];
     page4.desc = sampleDescription4;
+    page4.descColor = [UIColor blackColor];
     page4.bgImage = [UIImage imageNamed:@"bg4"];
     page4.titleIconView = [[UIImageView alloc] initWithImage:[UIImage imageNamed:@"title4"]];
     
     EAIntroView *intro = [[EAIntroView alloc] initWithFrame:self.view.bounds];
     [intro setDelegate:self];
-    
-    // show skipButton only on 3rd page + animation
+
     intro.skipButton.alpha = 0.f;
     intro.skipButton.enabled = NO;
-//    page3.onPageDidAppear = ^{
-//        intro.skipButton.enabled = YES;
-//        [UIView animateWithDuration:0.3f animations:^{
-//            intro.skipButton.alpha = 1.f;
-//        }];
-//    };
-//    page3.onPageDidDisappear = ^{
-//        intro.skipButton.enabled = NO;
-//        [UIView animateWithDuration:0.3f animations:^{
-//            intro.skipButton.alpha = 0.f;
-//        }];
-//    };
     
     [intro setPages:@[page1,page2,page3,page4]];
-    
     [intro showInView:self.view animateDuration:0.3];
-    
-    
 }
 
 - (void)introDidFinish:(EAIntroView *)introView{
