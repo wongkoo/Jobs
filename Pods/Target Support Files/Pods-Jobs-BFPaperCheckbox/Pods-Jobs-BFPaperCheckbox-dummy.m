b08f9e9673b88b72fe383228d101c512f3eef63a
wongkoo <wangzhenhuip@163.com>
2015-09-17 14:55:56 +0800

    Requires iOS 8.0 or later.
    For the release of iOS 9.0, Jobs no longer support iOS 7.0

--- Pods/Target Support Files/Pods-Jobs-BFPaperCheckbox/Pods-Jobs-BFPaperCheckbox-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_Jobs_BFPaperCheckbox : NSObject
-@end
-@implementation PodsDummy_Pods_Jobs_BFPaperCheckbox
-@end
--- Pods/Target Support Files/Pods-Jobs-MCSwipeTableViewCell/Pods-Jobs-MCSwipeTableViewCell-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_Jobs_MCSwipeTableViewCell : NSObject
-@end
-@implementation PodsDummy_Pods_Jobs_MCSwipeTableViewCell
-@end
