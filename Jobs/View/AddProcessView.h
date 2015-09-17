ea350c34567955eca6b9158e9811d5889129637a
wongkoo <wangzhenhuip@163.com>
2015-09-17 16:26:39 +0800

    Changed files in Finder rather than Group

---
+++ Jobs/View/AddProcessView.h
@@ -0,0 +1,13 @@
+//
+//  AddProcessView.h
+//  Jobs
+//
+//  Created by 王振辉 on 15/9/17.
+//  Copyright © 2015年 王振辉. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AddProcessView : UIView
+
+@end
---
+++ Jobs/View/AddProcessView.m
@@ -0,0 +1,13 @@
+//
+//  AddProcessView.m
+//  Jobs
+//
+//  Created by 王振辉 on 15/9/17.
+//  Copyright © 2015年 王振辉. All rights reserved.
+//
+
+#import "AddProcessView.h"
+
+@implementation AddProcessView
+
+@end
